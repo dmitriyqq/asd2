ab6aa8d804688ff001aff8f1c1bc99e4ee5d33de
dmitriyqq <shulaykin1@gmail.com>
2020-01-16 22:10:40 +0300

    Updates

---
+++ precache-manifest.7f52a1c10f1bb310da59f48abd93a15b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be83e3fdaa36c7a3ff29f4b41999c07c",
+    "url": "/asd2/index.html"
+  },
+  {
+    "revision": "de9f8dd74c006ab36a12",
+    "url": "/asd2/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cbdc6d20edf8661438a0",
+    "url": "/asd2/static/js/2.5dcc2905.chunk.js"
+  },
+  {
+    "revision": "0d4b6003882a68b3113f719496a978ff",
+    "url": "/asd2/static/js/2.5dcc2905.chunk.js.LICENSE"
+  },
+  {
+    "revision": "de9f8dd74c006ab36a12",
+    "url": "/asd2/static/js/main.ced43540.chunk.js"
+  },
+  {
+    "revision": "f2ce8e1cc35eafa7e96c",
+    "url": "/asd2/static/js/runtime-main.4e7b1dcb.js"
+  }
+]);
